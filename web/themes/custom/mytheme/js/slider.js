ddf0bf85fbe68bc3039275fad339a802315aad0b
abhaysiddhu1993@gmail.com <abhay.singh@qed42.com>
2022-09-05 10:36:22 +0530

    slider changes

--- web/themes/custom/mytheme/js/slider.js
+++ web/themes/custom/mytheme/js/slider.js
@@ -2,6 +2,57 @@
   "use strict";
   Drupal.behaviors.Slider = {
     attach() {
+      /* Start Home Page Banner Section */
+      var banner = ".view-carousel-slider .view-content ";
+      jQuery(banner).owlCarousel({
+        loop: true,
+        autoplay: false,
+        dots: true,
+        items: 1,
+      });
+      jQuery(banner).addClass("owl-carousel");
+      var testimonial = ".view-testimonial-carousel .view-content ";
+      jQuery(testimonial).owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        items: 1,
+      });
+      jQuery(testimonial).addClass("owl-carousel");
+      /* End Home Page Banner Section */
+
+      /* Start News & Events Section */
+      var blogs = ".blogs-section .view-news-and-events .view-content ";
+      jQuery(blogs).owlCarousel({
+        loop: true,
+        nav: true,
+        navText: [
+          "<div class='owl-prev'>prev</div>",
+          "<div class='owl-next'>next</div>",
+        ],
+        dots: false,
+        margin: 30,
+        items: 3,
+        responsiveClass: true,
+        responsive: {
+          0: {
+            items: 1,
+            nav: true,
+          },
+          768: {
+            items: 2,
+            nav: true,
+          },
+          1024: {
+            items: 3,
+            nav: true,
+          },
+        },
+      });
+      jQuery(blogs).addClass("owl-carousel");
+      /* End News & Events Section */
+
+      /* Start Footer Logo Auto Carousel */
       var logocarousel = ".view-logo-slider .view-content ";
       jQuery(logocarousel).owlCarousel({
         loop: true,
@@ -31,50 +82,7 @@
         },
       });
       jQuery(logocarousel).addClass("owl-carousel");
-      var blogs = ".blogs-section .view-news-and-events .view-content ";
-      jQuery(blogs).owlCarousel({
-        loop: true,
-        nav: true,
-        navText: [
-          "<div class='owl-prev'>prev</div>",
-          "<div class='owl-next'>next</div>",
-        ],
-        dots: false,
-        margin: 30,
-        items: 3,
-        responsiveClass: true,
-        responsive: {
-          0: {
-            items: 1,
-            nav: true,
-          },
-          768: {
-            items: 2,
-            nav: true,
-          },
-          1024: {
-            items: 3,
-            nav: true,
-          },
-        },
-      });
-      jQuery(blogs).addClass("owl-carousel");
-      var banner = ".view-carousel-slider .view-content ";
-      jQuery(banner).owlCarousel({
-        loop: true,
-        autoplay: false,
-        dots: true,
-        items: 1,
-      });
-      jQuery(banner).addClass("owl-carousel");
-      var testimonial = ".view-testimonial-carousel .view-content ";
-      jQuery(testimonial).owlCarousel({
-        loop: true,
-        autoplay: true,
-        dots: true,
-        items: 1,
-      });
-      jQuery(testimonial).addClass("owl-carousel");
+      /* End Footer Logo Auto Carousel */
     },
   };
 })(jQuery, Drupal);
